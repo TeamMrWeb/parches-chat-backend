b832845ce96634c2e28cec9a198056de59acb2ca
Manu <mcabral@frba.utn.edu.ar>
2023-01-18 00:04:31 -0300

    fix(utils): exist path

--- src/utils/files.js
+++ src/utils/files.js
@@ -1,18 +1,16 @@
-const fs = require('fs');
+const fs = require('fs')
 
 module.exports = {
     existsPath: async (path) => await fs.existsSync(path),
     clearPath: async (path) => {
-        const exists = await module.exports.existsPath(path);
-        if (!exists) return;
-        const files = await fs.readdirSync(path);
-        if(!files.length) return;
-        for (const file of files) 
-            await fs.unlinkSync(`${path}/${file}`)
+        const exists = await module.exports.existsPath(path)
+        if (!exists) return
+        const files = await fs.readdirSync(path)
+        if (!files.length) return
+        for (const file of files) await fs.unlinkSync(`${path}/${file}`)
     },
     createPath: async (path) => {
-        const exists = await existsPath(path);
-        if (!exists) await fs.mkdirSync(path);
+        const exists = await module.exports.existsPath(path)
+        if (!exists) await fs.mkdirSync(path)
     },
-    
 }
